ee7b2cbdd5d839e204780d12b8d85f3fd6f373f5
hirwajeaneric <hirwajeaneric25@gmail.com>
2024-04-09 11:27:53 +0200

    Feature: Added the setTime middleware

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,9 +1,12 @@
 import express from 'express';
 const taskRouter = express.Router();
-import { test, addTask, deleteTask, getTasks, findById, updateTask } from '../controllers/task.controller.js';
+import { test, addTask, deleteTask, getTasks, findById, updateTask, setTime } from '../controllers/task.controller.js';
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
 
 taskRouter.get('/test', test);
-taskRouter.post('/add', addTask);
+taskRouter.post('/add', setTime, addTask);
 taskRouter.get('/list', getTasks);
 taskRouter.put('/update', updateTask);
 taskRouter.get('/findById', findById);
